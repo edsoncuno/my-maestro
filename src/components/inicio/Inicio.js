aedca6137cccfd66e2d2bc812b0c5b611ccd1b1d
edsoncuno1@gmail.com <edsoncuno1@gmail.com>
2023-10-21 21:51:56 -0500

    actualizando inicio

--- src/components/inicio/Inicio.js
+++ src/components/inicio/Inicio.js
@@ -1,9 +1,9 @@
 import styles from "./Inicio.module.css";
 
 export default function App() {
-  /*
-  
-  <div className={styles.container}>
+  return (
+    <section id="section-inicio" className={styles.app}>
+      <div className={styles.container}>
         <div className={styles.carrusel}></div>
         <div className={styles.texto}>
           <h1 className={styles.titulo}>My Maestro</h1>
@@ -12,10 +12,6 @@ export default function App() {
           </p>
         </div>
       </div>
-  */
-  return (
-    <section id="section-inicio" className={styles.app}>
-      <div className={styles.container}></div>
     </section>
   );
 }
