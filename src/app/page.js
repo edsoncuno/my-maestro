49f2dcfcb5fc7c3fa2c299a135143c75703aed33
edsoncuno1@gmail.com <edsoncuno1@gmail.com>
2023-10-21 22:04:28 -0500

    agrego nosotros

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,19 @@
 import Inicio from "../components/inicio/Inicio";
-/*
 import Nosotros from "../components/nosotros/Nosotros";
+/*
 import Servicios from "../components/servicios/Servicios";
 import Contacto from "../components/contacto/Contacto";
 */
 
 export default function Home() {
   /*
-  <Nosotros />
       <Servicios />
       <Contacto />
   */
   return (
     <main>
       <Inicio />
+      <Nosotros />
     </main>
   );
 }
