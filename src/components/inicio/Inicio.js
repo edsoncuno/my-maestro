a87136dc9873062b8e59ff231c61310314b85a67
edsoncuno1@gmail.com <edsoncuno1@gmail.com>
2023-11-23 00:43:28 -0500

    actualizando inicio

--- src/components/inicio/Inicio.js
+++ src/components/inicio/Inicio.js
@@ -1,5 +1,15 @@
 import styles from "./Inicio.module.css";
 
 export default function App() {
-  return <section id="section-inicio" className={styles.app}></section>;
+  return (
+    <section id="section-inicio" className={styles.app}>
+      <div className={styles.redesSociales}>
+        <ul>
+          <li>
+            <a>asd</a>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
 }
