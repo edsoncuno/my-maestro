163455e223fd1b150dbc89b39a6bf9ddc33f3820
edsoncuno1@gmail.com <edsoncuno1@gmail.com>
2023-11-22 21:57:52 -0500

    se modifico el inicio

--- src/components/inicio/Inicio.js
+++ src/components/inicio/Inicio.js
@@ -1,17 +1,5 @@
 import styles from "./Inicio.module.css";
 
 export default function App() {
-  return (
-    <section id="section-inicio" className={styles.app}>
-      <div className={styles.container}>
-        <div className={styles.carrusel}></div>
-        <div className={styles.texto}>
-          <h1 className={styles.titulo}>My Maestro</h1>
-          <p className={styles.parrafo}>
-            Reparaciones e instalaciones con experiencia y dedicación
-          </p>
-        </div>
-      </div>
-    </section>
-  );
+  return <section id="section-inicio" className={styles.app}></section>;
 }
