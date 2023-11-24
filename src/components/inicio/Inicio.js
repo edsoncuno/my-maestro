ddeebe2f0b043b736750e04494a3f42911a4ac78
edsoncuno1@gmail.com <edsoncuno1@gmail.com>
2023-11-23 22:06:34 -0500

    redes sociales listo en inicio

--- src/components/inicio/Inicio.js
+++ src/components/inicio/Inicio.js
@@ -1,12 +1,20 @@
 import styles from "./Inicio.module.css";
 
 export default function App() {
+  //<div className={`${styles.slide} ${styles.imagen3}`}></div>
   return (
     <section id="section-inicio" className={styles.app}>
       <div className={styles.redesSociales}>
         <ul>
           <li>
-            <a>asd</a>
+            <a href="www.google.com" target="_blank" rel="noopener noreferrer">
+              <div className={styles.facebook}></div>
+            </a>
+          </li>
+          <li>
+            <a href="www.google.com" target="_blank" rel="noopener noreferrer">
+              <div className={styles.whatsapp}></div>
+            </a>
           </li>
         </ul>
       </div>
